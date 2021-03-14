76a6d88ddee81b7b8fea038e984a745c204acdd5
Jan Pecha <janpecha@email.cz>
2021-03-14 08:36:32 +0100

    Bundles: added FrontpackLessBundle

---
+++ src/Bundles/FrontpackLessBundle.php
@@ -0,0 +1,40 @@
+<?php
+
+	namespace Inteve\AssetsManager\Bundles;
+
+	use Inteve\AssetsManager\IAssetsBundle;
+	use Inteve\AssetsManager\Bundle;
+
+
+	class FrontpackLessBundle implements IAssetsBundle
+	{
+		/** @var string */
+		private $basePath;
+
+		/** @var string|NULL */
+		private $environment;
+
+
+		/**
+		 * @param string $basePath
+		 * @param string|NULL $environment
+		 */
+		public function __construct($basePath, $environment = 'development')
+		{
+			$this->basePath = $basePath . ($basePath !== '' ? '/' : '');
+			$this->environment = $environment;
+		}
+
+
+		public function getName()
+		{
+			return 'frontpack/less';
+		}
+
+
+		public function registerAssets(Bundle $bundle)
+		{
+			$bundle->addScript($this->basePath . 'less.config.js', $this->environment);
+			$bundle->addScript($this->basePath . 'less.js', $this->environment);
+		}
+	}
---
+++ tests/AssetsManager/Bundles/FrontpackLessBundle.phpt
@@ -0,0 +1,35 @@
+<?php
+
+use Inteve\AssetsManager\Bundle;
+use Inteve\AssetsManager\Bundler;
+use Inteve\AssetsManager\Bundles;
+use Tester\Assert;
+
+require __DIR__ . '/../../bootstrap.php';
+
+$bundler = new Bundler([]);
+
+test(function () use ($bundler) {
+	$netteFormsBundle = new Bundles\FrontpackLessBundle('');
+
+	$bundle = new Bundle($bundler);
+	$netteFormsBundle->registerAssets($bundle);
+
+	Assert::same([
+		'less.config.js',
+		'less.js',
+	], extractPaths($bundle->getScripts('development')));
+});
+
+
+test(function () use ($bundler) {
+	$netteFormsBundle = new Bundles\FrontpackLessBundle('assets/path', 'production');
+
+	$bundle = new Bundle($bundler);
+	$netteFormsBundle->registerAssets($bundle);
+
+	Assert::same([
+		'assets/path/less.config.js',
+		'assets/path/less.js',
+	], extractPaths($bundle->getScripts('production')));
+});
