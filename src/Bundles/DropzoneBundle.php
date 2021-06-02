33acd6fbf6c3fea1496227503994af2fd7a844a5
Jan Pecha <janpecha@email.cz>
2021-06-02 11:03:34 +0200

    Bundles: added DropzoneBundle

---
+++ src/Bundles/DropzoneBundle.php
@@ -0,0 +1,34 @@
+<?php
+
+	namespace Inteve\AssetsManager\Bundles;
+
+	use Inteve\AssetsManager\IAssetsBundle;
+	use Inteve\AssetsManager\Bundle;
+
+
+	class DropzoneBundle implements IAssetsBundle
+	{
+		/** @var string */
+		private $basePath;
+
+
+		/**
+		 * @param string $basePath
+		 */
+		public function __construct($basePath)
+		{
+			$this->basePath = $basePath . ($basePath !== '' ? '/' : '');
+		}
+
+
+		public function getName()
+		{
+			return 'enyo/dropzone';
+		}
+
+
+		public function registerAssets(Bundle $bundle)
+		{
+			$bundle->addScript($this->basePath . 'dropzone.js');
+		}
+	}
